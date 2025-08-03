3198d73f9ace75e3b66d1965605b1fc5b24858e0
Bruno Rezende <bruno.rezende1995@gmail.com>
2025-08-03 19:41:44 -0300

    CRUD Agendamento completo

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('agendamentos/', views.lista_agendamentos, name="lista_agendamentos"),
     path('agendamentos/novo', views.cadastro_agendamentos, name="cadastro_agendamentos"),
+    path('agendamentos/<int:id>/editar', views.editar_agendamentos, name="editar_agendamentos"),
+    path('agendamentos/<int:id>/excluir', views.excluir_agendamentos, name='excluir_agendamentos'),
     path('', views.total_agendamentos, name='home'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Agendamento
 
 def lista_agendamentos(request):
@@ -22,6 +22,22 @@ def cadastro_agendamentos(request):
         return redirect('lista_agendamentos')
     
     return render(request, 'core/cadastro_agendamentos.html')
+
+def editar_agendamentos(request, id):
+    agendamento = Agendamento.objects.get(id=id)
+    if request.method == 'POST':
+        agendamento.nome = request.POST.get('nome')
+        agendamento.data_e_hora_agendamento = request.POST.get('data_e_hora')
+        agendamento.telefone = request.POST.get('telefone')
+        agendamento.diagnostico = request.POST.get('diagnostico')
+        agendamento.save()
+        return redirect('lista_agendamentos')
+    return render(request, 'core/editar_agendamentos.html', {'agendamento': agendamento})
+
+def excluir_agendamentos(request, id):
+    agendamento = get_object_or_404(Agendamento, pk=id)
+    agendamento.delete()
+    return redirect('lista_agendamentos')
 
 def total_agendamentos(request):
     total_agendamentos = Agendamento.objects.count()
